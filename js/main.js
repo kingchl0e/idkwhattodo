02e012c6bdd163ee5064a0aff22466a733b7e063
Chloe Dalton <chloedalton@Chloes-MacBook-Pro-4.local>
2022-11-17 18:10:06 -0600

    added api

--- js/main.js
+++ js/main.js
@@ -2,29 +2,25 @@
   
  
   
-  const options = ['chat',
-                  'cheap',
-                  'cheaper',
-                  'cheapest',
-                  'cheat',
-                  'cheats',
-                  'check',
-                  'checked',
-                  'checking',
-                  'checklist',
-                  'checkout',
-                  'checks',
-                  'cheers',
-                  'cheese',
-                  'chef',
-                  'chem'];
+ 
+
                   
-  
-  function help(){
+  async function help(){
+    const req = await fetch('../text/wordlist1.txt');
+    const txt = await req.text();
+    const options = txt.split('\n');
     const randOptionNum = Math.floor(Math.random() * options.length);
+    const res = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+ options[randOptionNum])
+    const json = await res.json()
+    const meanings = json[0].meanings
+    console.log(meanings[0].definitions[0])
+  
+
     
     document.getElementById('doThis').innerHTML = '';
     document.getElementById('doThis').innerHTML = options[randOptionNum];
+    document.getElementById('definition').innterHTML = '';
+    document.getElementById('definition').innerHTML = meanings[0].definitions[0].definition[0];
   }
   
 	function writeWord (word) {
@@ -33,6 +29,10 @@
     document.body.style.fontSize = '10px'
     document.body.textContent = word
   }
-  
+
  
+
+
+
+
 
