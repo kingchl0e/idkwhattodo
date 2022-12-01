be915f4e76212b7127f6e0eb258c21f1c58e6651
Chloe Dalton <chloedalton@Chloes-MacBook-Pro-4.local>
2022-12-01 11:19:22 -0600

    display issues

--- js/main.js
+++ js/main.js
@@ -14,21 +14,47 @@
     const json = await res.json()
     const meanings = json[0].meanings
     console.log(meanings[0].definitions[0])
-  
+
+    const propmpts =
+    ['Write a poem using this word at the beginning of every line.',
+
+    'Describe how this word tastes.',
+    
+    'Create an anagram of this word.', 
+    
+    'Write a list poem using the letters of this word.'];
+
+    function prompt (){
+        const randOptionNum = Math.floor(Math.random() * options.length);
+    }
+    
 
     
     document.getElementById('doThis').innerHTML = '';
     document.getElementById('doThis').innerHTML = options[randOptionNum];
-    document.getElementById('definition').innterHTML = '';
-    document.getElementById('definition').innerHTML = meanings[0].definitions[0].definition[0];
+
+    document.getElementById('define').innterHTML = '';
+    document.getElementById('define').innerHTML = meanings[0].definitions[0].definition[0];
+    
+    
   }
-  
+
+
 	function writeWord (word) {
     document.body.style.marginTop = '50px'
     document.body.style.textAlign = 'center'
-    document.body.style.fontSize = '10px'
+    document.body.style.fontSize = '20px'
     document.body.textContent = word
   }
+
+  function writeDefinition (){
+    document.body.style.marginTop = '50px'
+    document.body.style.textAlign = 'center'
+    document.body.style.fontSize = '20px'
+    document.body.textContent = ''
+  }
+
+ 
 
  
 
